7713b40cdee851b4cba596113e229f16dc2650db
Nikita <nnazarov55@gmail.com>
2025-10-18 00:47:26 +0300

    added base form and libs for client auth

---
+++ src/components/forms/clientAuth/form.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { authClientShema } from './shema';
+import { onSubmit } from './action';
+import { Input } from '@/components/ui/input';
+
+const AuthClientForm = () => {
+  const form = useForm<z.infer<typeof authClientShema>>({
+    resolver: zodResolver(authClientShema),
+    defaultValues: {
+      username: '',
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default AuthClientForm;
