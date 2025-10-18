26bbc82603623926ad3f9116ad911333757cfb3f
Nikita <nnazarov55@gmail.com>
2025-10-18 17:03:23 +0300

    integrate shopify base auth client

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    'dev.nninc.uk',
+    'http://localhost:3000',
+    'http://localhost:3333',
+  ],
+  images: {
+    unoptimized: true,
+    domains: ['cdn.shopify.com'],
+    remotePatterns: [new URL('https://cdn.shopify.com')],
+  },
 };
 
 export default nextConfig;
---
+++ src/service/auth/auth.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { oauthShopifyClient } from './shopify/client';
+import { nextCookies } from 'better-auth/next-js';
+const betterAuthSecret = process.env.BETTER_AUTH_SECRET;
+const betterAuthUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!betterAuthSecret) {
+  throw new Error('BETTER_AUTH_SECRET environment variable is required');
+}
+if (!betterAuthUrl) {
+  throw new Error('BETTER_AUTH_URL environment variable is required');
+}
+
+export const auth = betterAuth({
+  secret: betterAuthSecret,
+  baseURL: betterAuthUrl,
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  trustedOrigins: [betterAuthUrl],
+  session: {
+    expiresIn: 60 * 60,
+    updateAge: 60,
+  },
+  plugins: [oauthShopifyClient, nextCookies()],
+});
