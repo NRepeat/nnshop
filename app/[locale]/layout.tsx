7777c5c426eb6abe8f3551d795f797eeeb2ff2e8
Nikita <nnazarov55@gmail.com>
2025-10-25 16:37:19 +0300

    init

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
 import { setRequestLocale } from 'next-intl/server';
+import { routing } from '~/src/i18n/routing';
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
