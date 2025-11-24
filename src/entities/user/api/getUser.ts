6bf5b236c8c62669207fff2984216bd38e998449
Nikita <nnazarov55@gmail.com>
2025-11-25 00:24:04 +0200

    update receipt

---
+++ src/entities/user/api/getUser.ts
@@ -0,0 +1,21 @@
+'use server';
+import { auth } from '@features/auth/lib/auth';
+import { prisma } from '@shared/lib/prisma';
+import { headers } from 'next/headers';
+
+const getUser = async () => {
+  try {
+    const session = await auth.api.getSession({ headers: await headers() });
+    if (!session) throw new Error('Unauthorized');
+    const user = await prisma.user.findUnique({
+      where: { id: session.user.id },
+    });
+    if (!user) return null;
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to get user');
+  }
+};
+
+export default getUser;
