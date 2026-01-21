1d9289398d57e5f992069dc444c01f54d473fe0a
Nikita <nnazarov55@gmail.com>
2026-01-21 23:11:40 +0200

    fix

--- app/[locale]/(frontend)/layout.tsx
+++ app/[locale]/(frontend)/layout.tsx
@@ -8,19 +8,6 @@ import { Header } from '@widgets/header/ui/Header';
 import { Footer } from '@widgets/footer/ui/Footer';
 import { locales } from '@shared/i18n/routing';
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-//   display: 'swap',
-//   fallback: [
-//     'system-ui',
-//     '-apple-system',
-//     'BlinkMacSystemFont',
-//     'Segoe UI',
-//     'Roboto',
-//     'sans-serif',
-//   ],
-// });
 const jostSans = Jost({
   variable: '--font-jost-sans',
   subsets: ['latin'],
@@ -35,13 +22,6 @@ const jostSans = Jost({
   ],
 });
 
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-//   display: 'swap',
-//   fallback: ['Consolas', 'Monaco', 'Courier New', 'monospace'],
-// });
-
 export const metadata: Metadata = {
   title: 'Mio Mio',
 };
@@ -52,32 +32,33 @@ export async function generateStaticParams() {
   }
   return params;
 }
-export default async function RootLayout({
-  children,
-  params,
-  auth,
-  modal,
-}: {
+interface RootProps {
   children: React.ReactNode;
-  modal: React.ReactNode;
-  auth: React.ReactNode;
   params: Promise<{ locale: string }>;
-}) {
+  modal?: React.ReactNode;
+  auth?: React.ReactNode;
+}
+
+export default async function RootLayout(props: RootProps) {
+  const { children, modal, auth, params } = props;
   const { locale } = await params;
+
   return (
-    <html lang={locale} suppressHydrationWarning className="">
-      <body className={`${jostSans.variable}  antialiased `}>
+    <html lang={locale} suppressHydrationWarning>
+      <body className={`${jostSans.variable} antialiased`}>
         <Providers>
-          <NextIntlClientProvider>
+          <NextIntlClientProvider locale={locale}>
             <Header locale={locale} />
+
             {children}
-            {modal}
-            {auth}
+
+            {modal && <div id="modal-slot">{modal}</div>}
+            {auth && <div id="auth-slot">{auth}</div>}
+
             <Footer locale={locale} />
           </NextIntlClientProvider>
         </Providers>
       </body>
-      <SpeedInsights />
     </html>
   );
 }
