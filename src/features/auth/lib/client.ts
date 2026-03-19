dd8ae7e80f31ba0f065e70c782317134749353d5
Nikita <95132082+NRepeat@users.noreply.github.com>
2026-03-20 00:18:15 +0200

    fix

--- src/features/auth/lib/client.ts
+++ src/features/auth/lib/client.ts
@@ -3,7 +3,6 @@ import { anonymousClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const client = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   plugins: [genericOAuthClient(), anonymousClient()],
 });
 
