4607b8fba5246be53f0a70b37845ce0f3506fbc2
Nikita <95132082+NRepeat@users.noreply.github.com>
2026-03-21 19:34:05 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
     viewTransition: true,
   },
   trailingSlash: false,
-  htmlLimitedBots: /.*/,
+  htmlLimitedBots:
+    /Googlebot|bingbot|Slurp|DuckDuckBot|Baiduspider|YandexBot|Sogou|facebot|ia_archiver/,
   output: 'standalone',
   productionBrowserSourceMaps: false,
   serverExternalPackages: [],
