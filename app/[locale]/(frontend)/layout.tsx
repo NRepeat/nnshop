f2385b95ab8c29c50a34f5a6a029ef25c0195c2e
Nikita <95132082+NRepeat@users.noreply.github.com>
2026-03-23 14:41:16 +0200

    feat: add Google Tag Manager (GTM-M2QHJZQF)

--- app/[locale]/(frontend)/layout.tsx
+++ app/[locale]/(frontend)/layout.tsx
@@ -127,6 +127,13 @@ export default async function RootLayout(props: RootProps) {
             });
           `}
         </Script>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M2QHJZQF');`}
+        </Script>
         <Script
           id="google-tag-manager"
           src="https://www.googletagmanager.com/gtag/js?id=G-05RL9JZJKK"
@@ -152,6 +159,14 @@ export default async function RootLayout(props: RootProps) {
         />
       </head>
       <body className={`${jostSans.variable} antialiased `}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2QHJZQF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           <ScrollDirectionProvider>
             <Header locale={locale} />
