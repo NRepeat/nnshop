f500d5ea55d43549159c45d22737ec6e8ee2964a
Nikita <95132082+NRepeat@users.noreply.github.com>
2026-04-03 16:39:30 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   experimental: {
-    cpus: 2,
+    // cpus: 2,
   },
   trailingSlash: false,
   output: 'standalone',
